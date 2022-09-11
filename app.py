d87fffc44dd08d972d07d41fc9d44f90b5da6768
hammy00612 <hammy00612@gmail.com>
2022-09-11 02:12:45 +0200

    scan 8

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 
 AWS_ACCESS_KEY_ID=A821HkfbA82lG5Jfds0KuFvHhahs
 AWS_SECRET_ACCESS_KEY=ha78617tGjAjGAu7gfaS12ghf
+POST / HTTP/2
+Host: HOST
+Accept: */*
+Content-Type: application/json
+GITHUB-PUBLIC-KEY-IDENTIFIER: 90a421169f0a406205f1563a953312f0be898d3c7b6c06b681aa86a874555f4a
+GITHUB-PUBLIC-KEY-SIGNATURE: MEQCIA6C6L8ZYvZnqgV0zwrrmRab10QmIFV396gsba/WYm9oAiAI6Q+/jNaWqkgG5YhaWshTXbRwIgqIK6Ru7LxVYDbV5Q==
+Content-Length: 0123
+
+[{"token":"NMIfyYncKcRALEXAMPLE","type":"mycompany_api_token","url":"https://github.com/octocat/Hello-World/blob/12345600b9cbe38a219f39a9941c9319b600c002/foo/bar.txt"}]
 
